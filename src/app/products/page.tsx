4ccee9dca9f6f19706661a921eae2d4344798294
mariana-codes <mariana@engineer.com>
2025-02-25 21:01:07 +0000

    feat: add shadcn setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const ProductPage = () => {
+  return (
+    <h1 className="text-red-500">
+      <Button>Button</Button>
+    </h1>
+  );
+};
+
+export default ProductPage;
