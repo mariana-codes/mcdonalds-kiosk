9a64749903fcd8941c4efbdb3aad012705667721
mariana-codes <mariana@engineer.com>
2025-02-28 15:23:09 +0000

    feat: add initial cart context and cart sheet component

--- src/app/[slug]/menu/[productId]/components/product-details.tsx
+++ src/app/[slug]/menu/[productId]/components/product-details.tsx
@@ -3,11 +3,14 @@
 import { Prisma } from "@prisma/client";
 import { ChefHatIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { formatCurrency } from "@/helpers/format-currency";
+
+import CartSheet from "../../components/cart-sheet";
+import { CartContext } from "../../contexts/cart";
 
 interface ProductDetailsProps {
   product: Prisma.ProductGetPayload<{
@@ -23,6 +26,7 @@ interface ProductDetailsProps {
 }
 
 const ProductDetails = ({ product }: ProductDetailsProps) => {
+  const { toggleCart } = useContext(CartContext);
   const [quantity, setQuantity] = useState<number>(1);
 
   const handleDecreaseQuantity = () => {
@@ -38,6 +42,8 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
   const handleIncreaseQuantity = () => {
     setQuantity((prev) => prev + 1);
   };
+
+  const handleAddToCart = () => toggleCart();
 
   return (
     <div className="relative z-50 mt-[-1.5rem] flex flex-auto flex-col overflow-hidden rounded-t-3xl p-5">
@@ -101,7 +107,10 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
           </div>
         </ScrollArea>
       </div>
-      <Button className="w-full rounded-full">Adicionar ao carrinho</Button>
+      <Button onClick={handleAddToCart} className="w-full rounded-full">
+        Adicionar ao carrinho
+      </Button>
+      <CartSheet />
     </div>
   );
 };
